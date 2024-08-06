e904a5931217d5620fc4409d86d53009a42aa5e7
Randú <134299499+joserandu@users.noreply.github.com>
2024-08-06 10:26:37 -0300

    Update lista6.cpp

--- Algoritmos e programação estruturada/lista6.cpp
+++ Algoritmos e programação estruturada/lista6.cpp
@@ -115,8 +115,34 @@ int main(void){
 
 	printf("\nExercício 4 ------------------------------------------------------------\n");
 	
+	/*
+	Ler 2 matrizes A e B, cada uma com 1D para 4 elementos. Criar
+	uma matriz C de 2D, onde a 1ª coluna da matriz C deverá ser formada
+	pelos elementos da matriz A multiplicados por 2 e a 2ª coluna deverá
+	ser formada pelos elementos da matriz B subtraídos de 5. Exibir a
+	matriz C.
+	*/
 	
+	int A4[4], B4[4], C4[4][2];
 	
-
+	// Formação das matrizes
+	for (i = 0; i<4; i++){
+		for (j = 0; j<3; j++){
+			
+			A4[i] = i;
+			B4[i] = i + 10;
+			
+			C4[i][0] = A4[i] * 2;
+			C4[i][1] = A4[i] - 5;
+			
+		}
+	}
+	
+	for (i = 0; i<4; i++){
+		for (j = 0; j<2; j++){
+			printf("%i ", C4[i][j]);
+		}
+		printf("\n");
+	}
 
 }
